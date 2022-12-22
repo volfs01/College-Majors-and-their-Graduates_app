ef91ccb30945f08d24e2eb96019d623ac22beed1
volfs01 <120348531+volfs01@users.noreply.github.com>
2022-12-22 17:35:59 +0900

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -1,4 +1,5 @@
 import streamlit as st 
+import webbrowser
 
 def run_home_app() :
     st.subheader('대학원졸, 대졸, 고졸의 취업,실업율,급여를 알 수 있고 수치를 비교해 그래프로 볼 수 있다. ')
@@ -6,3 +7,6 @@ def run_home_app() :
     st.image('https://post-phinf.pstatic.net/MjAxODAxMDFfMjAy/MDAxNTE0ODA5OTY0NzMw.E3My8KSuGs5dZ8Go5wiMpnisKmTrC6UExf1uutR5tpcg.xRShDZouqLrzOgxLDBPukm0I1Bd3kPlfFNU3UFngoEkg.JPEG/1280px-1_christ_church_hall_2012.jpg?type=w1200')
     
     st.sidebar.image('https://t1.daumcdn.net/cfile/tistory/270BF64954225AE919')
+    
+    if st.button('pca설명') :
+        webbrowser.open('https://www.kaggle.com/datasets/thedevastator/uncovering-insights-to-college-majors-and-their')
