96438d12d228eeb184a84fe8e0c11ece4ce8f900
volfs01 <120348531+volfs01@users.noreply.github.com>
2022-12-26 12:46:56 +0900

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import webbrowser
 
 def run_home_app() :
-    st.subheader('대학원졸, 대졸, 고졸의 취업,실업율,급여를 알 수 있고 수치를 비교해 그래프로 볼 수 있다. ')
+    st.subheader('!대학원졸, 대졸, 고졸의 취업,실업율,급여를 알 수 있고 수치를 비교해 그래프로 볼 수 있다. ')
     
     st.image('https://post-phinf.pstatic.net/MjAxODAxMDFfMjAy/MDAxNTE0ODA5OTY0NzMw.E3My8KSuGs5dZ8Go5wiMpnisKmTrC6UExf1uutR5tpcg.xRShDZouqLrzOgxLDBPukm0I1Bd3kPlfFNU3UFngoEkg.JPEG/1280px-1_christ_church_hall_2012.jpg?type=w1200')
     
